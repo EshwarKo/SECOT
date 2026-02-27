09dff729d83b0c2aacc845728f0dac4deabffa66
Amrita Suresh <amritasuresh2209@gmail.com>
2026-02-27 09:32:18 +0000

    removing files from other branches

--- secot/__init__.py
+++
@@ -1,6 +0,0 @@
-"""SECOT: Semantic Entropy for Chain-of-Thought analysis.
-
-Two methods for clustering CoT responses and computing semantic entropy:
-1. Key Step Method - Identify pivotal reasoning steps, cluster them, measure entropy
-2. Path Method - Compare full reasoning paths, cluster them, measure entropy
-"""
--- secot/clustering/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Clustering methods for chain-of-thought responses."""
-
-from secot.clustering.key_step import KeyStepClusterer
-from secot.clustering.path import PathClusterer
-
-__all__ = ["KeyStepClusterer", "PathClusterer"]
